cdb7011d9cf676035f4cd6cba5399237b0590a5b
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-16 12:23:37 +0330

    Import Vertex and Fragment files into our JS file using vite-plugin-glsl

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
-import * as THREE from 'three';
+//===================================================
+/* "It is not an actual project; therefore,
+I rely on comments to assess the code." */
+//===================================================
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
+import * as THREE from 'three';
+import vertex from './shaders/test/vertex.glsl';
+import fragment from './shaders/test/fragment.glsl';
 
 const canvas = document.querySelector('canvas.webgl');
 const gui = new GUI();
@@ -15,12 +21,8 @@ const textureLoader = new THREE.TextureLoader();
 //================== Objects ========================
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 const material = new THREE.RawShaderMaterial({
-  vertexShader: `
-    
-  `,
-  fragmentShaderL: `
-  
-  `,
+  vertexShader: vertex,
+  fragmentShader: fragment,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -66,3 +68,32 @@ const tick = () => {
 };
 
 tick();
+
+/*********** SyntaxError: Unexpected identifier 'mat4'
+ * this error may occur when we want to import our .glsl files
+ 
+ * Our project currently lacks the capability to properly process .glsl files. This issue arises because when our .glsl files are loaded, they are being treated as JavaScript files, which is incorrect. 
+ 
+* We need to extract the content of the .glsl files as a string and assign this string to a variable, which will then be used to pass the content to the Material.
+ 
+  There are two solutions to import glsl files: 
+  1. vite-plugin-glsl    // Easier to use
+  2.vite-plugin-glslify  // Standard
+
+  handy in 3 situation:
+    1. we want to split big shaders
+    2. we want to reuse shader chunks
+    3. we want to use external chunks made by other developers
+
+
+ - install: npm i vite-plugin-glsl --save-dev
+ - in vite.config.js :
+      import glsl from 'vite-plugin-glsl';
+      
+      plugins: [
+        glsl(), // Handle shader files
+      ],
+ */
+
+/************ map, alphaMap, opacity, color, etc
+ * they wont work on material and we need to write these features ourselves in vertex and fragment shaders */
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
 import restart from 'vite-plugin-restart';
+import glsl from 'vite-plugin-glsl';
+
+const isCodeSandbox = !(
+  'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+);
 
 export default {
   root: 'src/',
   publicDir: '../static/',
+
   server: {
     host: true, // Open to local network and display URL
-    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+    open: isCodeSandbox, // Open if it's not a CodeSandbox
   },
+
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     sourcemap: true,
   },
+
   plugins: [
-    restart({ restart: ['../static/**'] }), // Restart server on static file change
+    // Restart server on static file change
+    restart({ restart: ['../static/**'] }),
+    glsl(), // Handle shader files
   ],
 };
